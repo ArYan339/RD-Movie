a5d58afdea67eec6df25281d43ce2dfc9ef5eb97
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2025-02-17 10:08:48 -0600

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -48,7 +48,7 @@ url_list = {}
 
 def search_movies(query):
     movies_list = []
-    base_url = "https://prmovies.website"
+    base_url = "https://rkmovie.vercel.app/"
     search_url = f"{base_url}/?s={query.replace(' ', '+')}"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     
